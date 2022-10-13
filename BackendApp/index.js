a74c49ab9709ac8bc3200c0a0d33c929e564b881
lawandepranav <lawandepranav@gmail.com>
2022-10-13 17:25:40 +0530

    Update index.js

---
+++ BackendApp/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require ("cors")
+const connectionToDatabase= require('./Database/index');
+const setuser=require('./Handlers/users')
+const mutual=require('./Handlers/users')
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+connectionToDatabase().then(()=>{
+    app.listen(3030,()=>{
+        console.log("Server is running in 3030 port")
+    })
+})
